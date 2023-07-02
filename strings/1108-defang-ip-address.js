fec52cb8ce36836c3775ebdaeb4a69dd0a6d7999
rajat.sharma <rajatsharma@MacBook-Pro.local>
2023-07-02 15:21:00 +0530

    Problem 1108 - defang an IP address

---
+++ strings/1108-defang-ip-address.js
@@ -0,0 +1,71 @@
+/**
+ Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+ 
+
+Constraints:
+
+The given address is a valid IPv4 address.
+ */
+
+
+
+//Solution 2: My Solution
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    console.log("IP adress", address);
+    let output = "";
+    for (let i=0; i<address.length; i++) {
+        if (address[i] === ".") {
+            output += "[.]"
+        } else {
+            output += address[i];
+        }
+    }
+
+    return output.toString();
+};
+
+//Solution 2: 
+//Best solution with respect to time
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    return address.replaceAll('.','[.]')
+};
+
+
+//Solution 3: 
+//Best Solution in terms of memory
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function (s) {
+    let str = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ".") {
+            str += "[.]";
+        } else {
+            str += s[i];
+        }
+    }
+    return str;
+};
+
+
