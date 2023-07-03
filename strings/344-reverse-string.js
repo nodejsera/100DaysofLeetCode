a2da00fe4d9b95f24a2343e130595f8fa84b8ba7
rajat.sharma <rajatsharma@MacBook-Pro.local>
2023-07-04 00:51:02 +0530

    344. Reverse String

--- strings/344-reverse-string.js
+++ strings/344-reverse-string.js
@@ -14,3 +14,24 @@ Example 2:
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
  */
+
+//My Solution
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s =  s.reverse();
+ };
+
+ //Best Time solution
+//same as above. it is in top 10%
+
+ //Best Memory solution
+ /**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s.reverse()
+};
