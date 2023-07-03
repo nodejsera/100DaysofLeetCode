5df2fa6a0402e429d752b2ee39e038ad132e5081
rajat.sharma <rajatsharma@MacBook-Pro.local>
2023-07-04 00:47:29 +0530

    28 find index of the first occurence

---
+++ strings/28-find-index-of-first-occurence.js
@@ -0,0 +1,58 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+ */
+
+//My Solution
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    const abc = haystack.search(needle)
+    return abc
+};
+
+//Best time solution
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+//Best Memory solution
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+
+    let index = haystack.indexOf(needle);
+
+    return index;
+    
+};
