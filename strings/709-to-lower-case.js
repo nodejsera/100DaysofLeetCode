e716bdf048c63a1e87e34410aca5fb96bed17448
Rajat Sharma <rajatsharma1492@gmail.com>
2023-07-05 23:08:03 +0530

    Create 709-to-lower-case.js

---
+++ strings/709-to-lower-case.js
@@ -0,0 +1,46 @@
+/**
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+*/
+
+//My solution: 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+
+//Best in Time: 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase()
+    
+};
+
+//Best in Memory: 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase()
+};
